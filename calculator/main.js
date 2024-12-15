869381aa6a227fb90881673c6a156dd7cc41e3c8
Nikolaichik_Alexey <rogroty@gmail.com>
2024-12-15 16:18:05 +0400

    Добавлена логика изменения размера калькулятора при вводе длинного числа

--- calculator/main.js
+++ calculator/main.js
@@ -4,49 +4,57 @@ let buttons = Array.from(document.querySelectorAll('.button'));
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        switch(e.target.innerText) {
 
-            case 'AC':
-                display.innerText = '0';
-                break;
+        if (display.innerText.length > 12) {
+            document.querySelector('.calculator').style.height = '750px';
+        }
 
-            case '=':
-                try {
-                    let result = eval(display.innerText).toString();
-                    if (result.indexOf('.') > -1) {
-                        display.innerText = cutNum(result);
-                    } else {
-                        display.innerText = result;
+        if (display.innerText.length < 24) {
+            switch(e.target.innerText) {
+
+                case 'AC':
+                    display.innerText = '0';
+                    break;
+
+                case '=':
+                    try {
+                        let result = eval(display.innerText).toString();
+                        if (result.indexOf('.') > -1) {
+                            display.innerText = cutNum(result);
+                        } else {
+                            display.innerText = result;
+                        }
+                        
+                        
+                    } catch(e) {
+                        display.innerText = e;
                     }
-                     
+                    break;
+
+                case '%':
+                    let passedText = display.innerText + '/100';
+                    display.innerText = eval(passedText);
+                    break;
+
+                case '+/-':
+                    display.innerText = -display.innerText;
+                    break;
+
+                default:
+                    if (display.innerText === '0' && e.target.innerText != ".") {
+                        display.innerText = e.target.innerText;
+                    } else {
+                        display.innerText += e.target.innerText;
+                    }
                     
-                } catch(e) {
-                    display.innerText = e;
-                }
-                break;
-
-            case '%':
-                let passedText = display.innerText + '/100';
-                display.innerText = eval(passedText);
-                break;
-
-            case '+/-':
-                display.innerText = -display.innerText;
-                break;
-
-            default:
-                if (display.innerText === '0' && e.target.innerText != ".") {
-                    display.innerText = e.target.innerText;
-                } else {
-                    display.innerText += e.target.innerText;
-                }
-                
+            }
         }
     });
 
     
 })
 
+// Функция обрезает дробную часть, если её длина больше 4х
 function cutNum(num) { 
     let decimal = num.split('.');
     if (decimal[1].length > 4) {
